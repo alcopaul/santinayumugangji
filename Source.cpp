8d643bf9795b8a4e12c3c33261daac5e8ff9e20e
Paul Glenerson B. Amurao <paul.amurao@gmail.com>
2016-03-29 13:25:50 -0400

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+extern "C" char * DDD(char * h, int j, int key);
+
+int main()
+{
+	char g[] = "DRAGON";
+	cout << "Crypted message is " << DDD(g, (int) strlen(g), 153) << endl;
+	_getch();
+	cout << "Decrypted message is " << DDD(g, (int) strlen(g), 153) << endl;
+	_getch();
+	return 0;
+}
