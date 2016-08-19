8916f670bfc534eb426a14a167a32541b80e1208
VP7O <paul.amurao@gmail.com>
2016-08-19 19:38:15 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@ extern "C" char * DDD(char * h, int j, int key);
 
 int main()
 {
-	char g[] = "DRAGON";
+	char g[] = "DRAGON.";
 	cout << "Crypted message is " << DDD(g, (int) strlen(g), 153) << endl;
 	_getch();
 	cout << "Decrypted message is " << DDD(g, (int) strlen(g), 153) << endl;
