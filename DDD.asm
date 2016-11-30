7891bc8bb28aaed82acab6654da98f9f9d1ffd2b
kyanos <paul.amurao@gmail.com>
2016-11-30 14:11:06 -0500

    Update DDD.asm

--- DDD.asm
+++ DDD.asm
@@ -1,15 +1,15 @@
 .code
 DDD proc
-lea rbx, [rcx]
-push rbx
+lea r9, [rcx]
+push r9
 a20:
-xor byte ptr [rbx], r8b
-inc rbx
+xor byte ptr [r9], r8b
+inc r9
 dec rdx
 cmp rdx, 0
 jnz a20
-pop rbx
-mov rax, rbx
+pop r9
+mov rax, r9
 ret
 DDD endp
 end
